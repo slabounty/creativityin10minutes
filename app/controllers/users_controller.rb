296a4fae8cb7cb3e7dd35b810bbbcd5f96123562
Scott LaBounty <slabounty@gmail.com>
2026-07-13 18:39:36 -0700

    Add Splash and Signup
    * Add splash and signup screens
    * Add a home screen where we'll show today's prompt

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  allow_unauthenticated_access
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      start_new_session_for @user
+      redirect_to home_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
+  end
+end
