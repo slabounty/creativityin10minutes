b252618c0e7ad4870fe6e6975c3cf65886d55308
Scott LaBounty <slabounty@gmail.com>
2026-07-25 19:43:34 -0700

    Implemnt Daily Prompt
    * Add for_today in DailyPrmpt
    * Use in the home controller and ...
    * set prompt and media from that
    * Add test

--- app/models/daily_prompt.rb
+++ app/models/daily_prompt.rb
@@ -4,4 +4,11 @@ class DailyPrompt < ApplicationRecord
 
   validates :prompt_date, presence: true
   validates :prompt_date, uniqueness: true
+
+  def self.for_today
+    find_or_create_by!(prompt_date: Date.current) do |daily_prompt|
+      daily_prompt.prompt = Prompt.random
+      daily_prompt.medium = Medium.random
+    end
+  end
 end
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,3 +1,7 @@
 class Medium < ApplicationRecord
   has_many :daily_prompts, dependent: :restrict_with_exception
+
+  def self.random
+    order(Arel.sql("RANDOM()")).first
+  end
 end
